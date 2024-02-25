ed69d0191ed2124001a4d4026ee83388f3a1bc02
pooria <35379077+pooriaashkevarvakili@users.noreply.github.com>
2024-02-25 08:06:29 +0330

    pooria
    update

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+      <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
